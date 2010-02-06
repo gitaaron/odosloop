0105143389ce0f0a7b85dc9798429ade343af3ae
Aaron Surty <aaron@zompf.com>
2010-02-05 23:28:46 -0500

    removing older files

--- sodalabs/playlistia/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
-
-urlpatterns = patterns('sodalabs.playlistia.views',
-        (r'^$', direct_to_template, {'template':'playlistia/index.html'}),
-        (r'^user/$', 'profile'),
-        (r'^user/(?P<username>.+)/$', 'profile'),
-        (r'^open/$', 'open'),
-)
