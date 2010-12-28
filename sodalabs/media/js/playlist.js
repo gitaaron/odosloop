53d892c713449db16cffc416689d02cffd131871
Aaron Surty <aaron.surty@gmail.com>
2010-12-27 22:54:47 -0500

    loggin

--- sodalabs/media/js/playlist.js
+++ sodalabs/media/js/playlist.js
@@ -89,6 +89,7 @@ var Playlist = {
             password = localStorage.getItem('gtalk_password');
             if(username && password) {
                 $.post('/accounts/gtalk/' + lastFMTrackSongId + '/', {'username':username, 'password':password});
+                console.log('post');
             } else {
                 console.log('no username or pwd');
             }
